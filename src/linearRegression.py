f1e65894cd9afa2581e3834cd7119fe94ff8d203
Maryam Dabaghchian <maryam@maryams-mbp.uconnect.utah.edu>
2016-12-08 11:28:40 -0700

    added linear regression

---
+++ src/linearRegression.py
@@ -0,0 +1,37 @@
+import numpy as np
+from numpy import linalg
+import math
+import csv
+
+def get_x(line):
+    ## Add bias to x at index 0
+    newLine = [1.0]
+    for i in range(1, len(line)):
+        newLine.append(float(line[i]))
+    return newLine
+
+def MatricesXY(Data):
+    X = []
+    Y = []
+    for row in Data:
+        x= get_x(row)
+        X.append(x)
+        y = float(row[0])
+        Y.append(y)
+    return np.array(X), np.array(Y)
+
+##------- Compute the pseudo inverse of X -------##
+def PseudoInverse(X):
+    X_T    = np.array(X.transpose())
+    M      = np.dot(X_T, X)
+    print M
+    M_inv  = linalg.inv(M)
+    X_pinv = np.dot(M_inv, X_T)
+    return X_pinv
+##-----------------------------------------------##
+
+def LinearRegression(Data):
+   X, Y   = MatricesXY(Data)
+   X_pinv = linalg.pinv(X)
+   #PseudoInverse(X)
+   return np.dot(X_pinv, Y)
--- src/main.py
+++ src/main.py
@@ -1,29 +1,68 @@
-
+import csv
 import sys
 import re
 import copy
-import func
 import math
 import numpy
 import os
 
+##--- import app libs ---##
+import func
+import linearRegression
+
+genId   = -1
 trainId = -1
+learnId = -1
+
+Dpath     = ""
+BMList    = []
+BMSubList = []
+FScore    = []
 
 if("-trainDir" in sys.argv):
 	trainId = sys.argv.index("-trainDir")
 else:
 	print "No Training. Use already trained weights"
 
+if("-gen" in sys.argv):
+        genId = sys.argv.index("-gen")
+
+if("-learn" in sys.argv):
+        learnId = sys.argv.index("-learn")
+        
 if(trainId != -1):
-	Dpath        = os.getcwd()+"/../Data/"+sys.argv[trainId + 1]+"/"
+	Dpath     = os.getcwd()+"/../Data/"+sys.argv[trainId + 1]+"/"
 	print Dpath+sys.argv[trainId + 2]
 	BMList    = open(Dpath+sys.argv[trainId + 2], 'r+').read().splitlines()
 	BMSubList = open(Dpath+sys.argv[trainId + 3], 'r+').read().splitlines()
 	FScore    = open(Dpath+sys.argv[trainId + 4], 'r+').read().splitlines()
+
 else:
 	##--- Add code for using learned weights ---
 	print "Use Learned weights"
 
-func.generateTrainingData(BMList, BMSubList, FScore, Dpath)
+if(genId != -1):
+        func.generateTrainingData(BMList, BMSubList, FScore, Dpath)
 
+##------ Train on every tool -------##
+if(learnId != -1):
+        Tools = []
+	
+        if 'FScore' in locals():
+                Tools = FScore[0].split(":")[1].split()
+        else:
+                Dpath  = os.getcwd()+"/../Data/"+sys.argv[trainId + 1]+"/"
+      	        FScore = open(Dpath+sys.argv[trainId + 4], 'r+').read().splitlines()
+                Tools  = FScore[0].split(":")[1].split()
 
+        w = {} #[tool -> weight([index -> val])]
+        for tool in Tools:
+                ##--------- Get the training data ---------##
+                trainFileName = Dpath+"/Training/"+tool+".data"
+
+                trainData = csv.reader(open(trainFileName, "rb"), delimiter=" ")
+
+                w_temp = linearRegression.LinearRegression(trainData)
+                w[tool] = w_temp
+
+        print "w:" , w
